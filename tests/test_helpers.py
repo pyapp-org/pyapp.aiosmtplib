8e9f9b26e20972516adbc4fe79cf6e72d09e197c
Tim Savage <tim@savage.company>
2020-06-16 00:54:35 +1000

    Added test

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,1 +1,13 @@
+from unittest.mock import Mock
+
 from pyapp_ext.aiosmtplib import helpers
+
+
+class TestEmail:
+    def test_init(self, monkeypatch):
+        mock_factory = Mock()
+        monkeypatch.setattr(helpers, "get_client", mock_factory)
+
+        helpers.Email(name="foo")
+
+        mock_factory.assert_called_with("foo")
